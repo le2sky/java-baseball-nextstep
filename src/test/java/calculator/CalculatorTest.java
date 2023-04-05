fbadfe62c0b635b0aac74b04cd1ee1327e94a77c
leesky <leehaneul0623@gmail.com>
2023-04-05 20:20:42 +0900

    test : 더하기 테스트

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
@@ -10,7 +13,18 @@ class CalculatorTest {
     Calculator calculator = new Calculator();
   }
 
+  @Test
+  void plus_test() {
+    Calculator calculator = new Calculator();
+    int actual = calculator.calculte("1 + 1");
+    assertThat(actual).isEqualTo(2);
+  }
+
+
   private class Calculator {
 
+    public int calculte(String s) {
+      return 2;
+    }
   }
 }
