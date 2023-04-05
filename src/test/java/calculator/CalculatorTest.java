352a3b02b5ce154a3e8c038e15b6af6de81ffd25
leesky <leehaneul0623@gmail.com>
2023-04-05 20:18:07 +0900

    test : calculator 생성 테스트

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,16 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+
+  @Test
+  void create_calculator() {
+    Calculator calculator = new Calculator();
+  }
+
+  private class Calculator {
+
+  }
+}
