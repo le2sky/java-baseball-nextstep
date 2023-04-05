bcb0232217f24e578a109246e66db2111305c9aa
leesky <leehaneul0623@gmail.com>
2023-04-05 20:34:33 +0900

    test : 다항 더하기 테스트

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -2,6 +2,8 @@ package calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -22,15 +24,27 @@ class CalculatorTest {
   }
 
   @Test
+  void calculate_피연산자_세개_더하기() {
+    int actual = calculator.calculate("1 + 1 + 1");
+    assertThat(actual).isEqualTo(3);
+  }
+  @Test
   void calculate_피연산자_두개_빼기() {
     int actual = calculator.calculate("1 - 1");
     assertThat(actual).isEqualTo(0);
   }
+
+
   private static class Calculator {
 
     public int calculate(String expression) {
       if (expression.contains("+")) {
-        return 2;
+        return Arrays.stream(expression.split(" "))
+            .filter(s -> !s.equals("+"))
+            .collect(Collectors.toList()).stream()
+            .mapToInt(Integer::parseInt)
+            .sum();
+
       } else {
         return 0;
       }
