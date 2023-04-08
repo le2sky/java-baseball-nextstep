2edfc644d049c41fef43cf9e9aad0af2898abfba
leesky <leehaneul0623@gmail.com>
2023-04-08 18:38:02 +0900

    test : 3스트라이크 테스트

--- src/test/java/baseballgame/BaseballGameTest.java
+++ src/test/java/baseballgame/BaseballGameTest.java
@@ -1,5 +1,7 @@
 package baseballgame;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 class BaseballGameTest {
@@ -9,7 +11,17 @@ class BaseballGameTest {
         BaseballGame baseballGame = new BaseballGame();
     }
 
-    private class BaseballGame {
+    @Test
+    void judge_모든_수가_정답과_모두_같은_자리에_있으면_스트라이크() {
+        BaseballGame baseballGame = new BaseballGame();
+        String actual = baseballGame.judge("123");
+        assertThat(actual).isEqualTo("3스트라이크");
+    }
 
+    private static class BaseballGame {
+
+        public String judge(String s) {
+            return "3스트라이크";
+        }
     }
 }
