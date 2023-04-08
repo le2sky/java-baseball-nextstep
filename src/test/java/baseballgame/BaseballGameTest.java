7bda571341f39f5efbbdc1292282ac422041a584
leesky <leehaneul0623@gmail.com>
2023-04-08 18:43:30 +0900

    refactor : 중복 코드 정리

--- src/test/java/baseballgame/BaseballGameTest.java
+++ src/test/java/baseballgame/BaseballGameTest.java
@@ -2,27 +2,31 @@ package baseballgame;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class BaseballGameTest {
 
-    @Test
-    void create_baseballgame() {
-        BaseballGame baseballGame = new BaseballGame();
+    private static BaseballGame baseballGame;
+
+    private static void assertJudge(String source, String expected) {
+        String actual = baseballGame.judge(source);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @BeforeAll
+    static void setUp() {
+        baseballGame = new BaseballGame();
     }
 
     @Test
     void judge_모든_수가_정답과_모두_같은_자리에_있으면_3스트라이크() {
-        BaseballGame baseballGame = new BaseballGame();
-        String actual = baseballGame.judge("123");
-        assertThat(actual).isEqualTo("3스트라이크");
+        assertJudge("123", "3스트라이크");
     }
 
     @Test
     void judge_두가지_수가_정답과_같은_자리에_있으면_2스트라이크() {
-        BaseballGame baseballGame = new BaseballGame();
-        String actual = baseballGame.judge("153");
-        assertThat(actual).isEqualTo("2스트라이크");
+        assertJudge("153", "2스트라이크");
     }
 
     private static class BaseballGame {
