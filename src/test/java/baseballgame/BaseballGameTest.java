9aac29b6fe05746b0fdcb39a0bbec9e06fdaed32
leesky <leehaneul0623@gmail.com>
2023-04-08 18:34:26 +0900

    test : 야구게임 생성 테스트

---
+++ src/test/java/baseballgame/BaseballGameTest.java
@@ -0,0 +1,15 @@
+package baseballgame;
+
+import org.junit.jupiter.api.Test;
+
+class BaseballGameTest {
+
+    @Test
+    void create_baseballgame() {
+        BaseballGame baseballGame = new BaseballGame();
+    }
+
+    private class BaseballGame {
+
+    }
+}
