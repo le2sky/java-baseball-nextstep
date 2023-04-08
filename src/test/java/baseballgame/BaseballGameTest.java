09619d7016b1af74697ed6e8b2a674e18066cd1a
leesky <leehaneul0623@gmail.com>
2023-04-08 18:39:49 +0900

    test : 2스트라이크 테스트

--- src/test/java/baseballgame/BaseballGameTest.java
+++ src/test/java/baseballgame/BaseballGameTest.java
@@ -12,15 +12,25 @@ class BaseballGameTest {
     }
 
     @Test
-    void judge_모든_수가_정답과_모두_같은_자리에_있으면_스트라이크() {
+    void judge_모든_수가_정답과_모두_같은_자리에_있으면_3스트라이크() {
         BaseballGame baseballGame = new BaseballGame();
         String actual = baseballGame.judge("123");
         assertThat(actual).isEqualTo("3스트라이크");
     }
 
+    @Test
+    void judge_두가지_수가_정답과_같은_자리에_있으면_2스트라이크() {
+        BaseballGame baseballGame = new BaseballGame();
+        String actual = baseballGame.judge("153");
+        assertThat(actual).isEqualTo("2스트라이크");
+    }
+
     private static class BaseballGame {
 
         public String judge(String s) {
+            if (s.equals("153")) {
+                return "2스트라이크";
+            }
             return "3스트라이크";
         }
     }
