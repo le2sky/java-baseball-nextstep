8a19ade18bdaf880ce6990de489ee446014fc6fb
leesky <leehaneul0623@gmail.com>
2023-04-23 01:32:08 +0900

    feat : balls - 같은 수가 다른 자리에 있다면 볼

--- src/test/java/baseballgamev2/BallsTest.java
+++ src/test/java/baseballgamev2/BallsTest.java
@@ -3,6 +3,7 @@ package baseballgamev2;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import baseballgamev2.BallTest.Ball;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -16,13 +17,35 @@ class BallsTest {
         assertThat(balls.play(target)).isEqualTo(BallStatus.NOTHING);
     }
 
+    @Test
+    void 같은_수가_다른_자리에_있다면_볼() {
+        Balls balls = new Balls(Arrays.asList(1, 2, 3));
+        Ball target = new Ball(3, 1);
+        assertThat(balls.play(target)).isEqualTo(BallStatus.BALL);
+    }
+
     public static class Balls {
 
-        public <T> Balls(List<T> list) {
+        private final List<Ball> balls;
+
+        public Balls(List<Integer> list) {
+            this.balls = mapToBalls(list);
+
+        }
+
+        private List<Ball> mapToBalls(List<Integer> list) {
+            List<Ball> balls = new ArrayList<>();
+            for (int i = 0; i < list.size(); i++) {
+                balls.add(new Ball(i + 1, list.get(i)));
+            }
+            return balls;
         }
 
         public BallStatus play(Ball ball) {
-            return BallStatus.NOTHING;
+            return balls.stream()
+                .map(ball::matchWith)
+                .findFirst()
+                .orElse(BallStatus.NOTHING);
         }
     }
 }
