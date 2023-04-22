503a1cf5cbe84267015d55bfb13149d482d7363d
leesky <leehaneul0623@gmail.com>
2023-04-23 00:05:54 +0900

    feat : 숫자는 세자리의 숫자이다

--- src/test/java/baseballgamev2/ValidationUtilTest.java
+++ src/test/java/baseballgamev2/ValidationUtilTest.java
@@ -11,10 +11,10 @@ class ValidationUtilTest {
 
     @Test
     void 숫자는_1에서9사이의_수_이다() {
-        assertThat(validate(1)).isTrue();
-        assertThat(validate(9)).isTrue();
-        assertThat(validate(10)).isFalse();
-        assertThat(validate(0)).isFalse();
+        assertThat(validateEach(1)).isTrue();
+        assertThat(validateEach(9)).isTrue();
+        assertThat(validateEach(10)).isFalse();
+        assertThat(validateEach(0)).isFalse();
     }
 
     @Test
@@ -23,11 +23,39 @@ class ValidationUtilTest {
         assertThat(validate(Arrays.asList(1, 2, 1))).isFalse();
     }
 
-    private boolean validate(List<Integer> list) {
-        return new HashSet<>(list).size() == list.size();
+    @Test
+    void 숫자는_3자리의_숫자이다() {
+        assertThat(validate(Arrays.asList(1, 2, 3))).isTrue();
+        assertThat(validate(Arrays.asList(1, 2, 3, 4))).isFalse();
     }
 
-    private boolean validate(int i) {
+    @Test
+    void 숫자는_서로_다른_세자리_의_1부터_9_사이의_숫자이다() {
+        assertThat(validate(Arrays.asList(1, 2, 3))).isTrue();
+        assertThat(validate(Arrays.asList(1, 0, 3))).isFalse();
+        assertThat(validate(Arrays.asList(1, 3))).isFalse();
+        assertThat(validate(Arrays.asList(9, 0))).isFalse();
+    }
+
+    private boolean validate(List<Integer> list) {
+        return validateSize(countInRange(list)) && validateDuplicate(list);
+    }
+
+    private boolean validateSize(long count) {
+        return count == 3;
+    }
+
+    private long countInRange(List<Integer> list) {
+        return list.stream()
+            .filter(this::validateEach)
+            .count();
+    }
+
+    private boolean validateEach(int i) {
         return i <= 9 && i >= 1;
+    }
+
+    private boolean validateDuplicate(List<Integer> list) {
+        return new HashSet<>(list).size() == list.size();
     }
 }
