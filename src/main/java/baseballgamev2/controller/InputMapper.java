17c75f8befc1decd38e6b63d12c3d133de61d652
leesky <leehaneul0623@gmail.com>
2023-05-08 01:45:50 +0900

    refactor : static import 제거

--- src/main/java/baseballgamev2/controller/InputMapper.java
+++ src/main/java/baseballgamev2/controller/InputMapper.java
@@ -1,15 +1,15 @@
 package baseballgamev2.controller;
 
-import static java.util.Arrays.stream;
 
 import baseballgamev2.domain.Balls;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class InputMapper {
 
     static Balls mapToBalls(String input) {
-        List<Integer> target = stream(input.split(""))
+        List<Integer> target = Arrays.stream(input.split(""))
             .map(Integer::parseInt)
             .collect(Collectors.toUnmodifiableList());
 
