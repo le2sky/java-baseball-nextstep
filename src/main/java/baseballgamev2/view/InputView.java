0c1c66b7d822555519b0ed29bb5053500f8fe557
leesky <leehaneul0623@gmail.com>
2023-05-08 01:42:42 +0900

    refactor : rename constant

--- src/main/java/baseballgamev2/view/InputView.java
+++ src/main/java/baseballgamev2/view/InputView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InputView {
 
     private static final String READ_USER_BALLS_MESSAGE = "숫자를 입력해 주세요 : ";
-    private static final String READ_USER_GAME_COMMAND = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String READ_USER_GAME_COMMAND_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public String readBalls() {
         System.out.print(READ_USER_BALLS_MESSAGE);
@@ -13,7 +13,7 @@ public class InputView {
     }
 
     public String readReplayCommand() {
-        System.out.println(READ_USER_GAME_COMMAND);
+        System.out.println(READ_USER_GAME_COMMAND_MESSAGE);
         return readLineWithOutBlank();
     }
 
