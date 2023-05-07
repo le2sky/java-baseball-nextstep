40d28c3748ab6d18b06c30c0248458489b41714c
leesky <leehaneul0623@gmail.com>
2023-05-07 23:58:49 +0900

    feat : ball, strike 출력 기능 구현

---
+++ src/main/java/baseballgamev2/view/ResultView.java
@@ -0,0 +1,35 @@
+package baseballgamev2.view;
+
+import baseballgamev2.domain.PlayResult;
+
+public class ResultView {
+
+    private static final String WRITE_STRIKE_FORMAT = "%d스트라이크";
+    private static final String WRITE_BALL_FORMAT = "%d볼";
+    private static final String WRITE_NOTHING_MESSAGE = "낫싱";
+    private static final String WRITE_GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public void writeResult(PlayResult result) {
+        StringBuilder message = new StringBuilder();
+        appendBallMessage(result, message);
+        appendStrikeMessage(result, message);
+
+        System.out.println(message);
+    }
+
+    private void appendStrikeMessage(PlayResult result, StringBuilder stringBuilder) {
+        int strike = result.getStrike();
+        if (strike == 0) {
+            return;
+        }
+        stringBuilder.append(String.format(WRITE_STRIKE_FORMAT, strike));
+    }
+
+    private void appendBallMessage(PlayResult result, StringBuilder stringBuilder) {
+        int ball = result.getBall();
+        if (ball == 0) {
+            return;
+        }
+        stringBuilder.append(String.format(WRITE_BALL_FORMAT, ball));
+    }
+}
